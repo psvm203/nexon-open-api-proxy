00f3c303a5923300b4c91e0aa768cff55ff6f0d6
psvm203 <psvm203@proton.me>
2025-08-06 19:27:23 +0900

    feat: implement nexon open api proxy

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,22 @@
 use worker::*;
 
+const API_KEY: &str = include_str!("key.txt");
+const ORIGIN: &str = "https://open.api.nexon.com";
+
 #[event(fetch)]
-async fn fetch(
-    _req: Request,
-    _env: Env,
-    _ctx: Context,
-) -> Result<Response> {
-    console_error_panic_hook::set_once();
-    Response::ok("Hello World!")
+async fn fetch(req: Request, _env: Env, _ctx: Context) -> Result<Response> {
+    let mut url = Url::parse(ORIGIN)?;
+    url.set_path(req.url()?.path());
+    url.set_query(req.url()?.query());
+
+    let mut request = Request::new(url.as_str(), Method::Get)?;
+    let request_headers = request.headers_mut()?;
+    request_headers.set("accept", "application/json")?;
+    request_headers.set("x-nxopen-api-key", API_KEY)?;
+
+    let mut response = Fetch::Request(request).send().await?.cloned()?;
+    let response_headers = response.headers_mut();
+    response_headers.set("Access-Control-Allow-Origin", "*")?;
+
+    Ok(response)
 }
