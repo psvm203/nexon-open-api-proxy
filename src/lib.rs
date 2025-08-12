46ff9fb57feef0ebade21a58a0c563f061569807
psvm203 <psvm203@proton.me>
2025-08-12 22:16:06 +0900

    chore: set up panic hook for better error reports

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,8 @@ const CACHE_TTL: u32 = 300;
 
 #[event(fetch)]
 async fn fetch(req: Request, _env: Env, _ctx: Context) -> Result<Response> {
+    console_error_panic_hook::set_once();
+
     let mut url = Url::parse(ORIGIN)?;
     url.set_path(req.url()?.path());
     url.set_query(req.url()?.query());
